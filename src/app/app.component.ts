6f782700068ef19056c9ec88304429fb4374e42e
Anindya Biswas <anindya31@gmail.com>
2022-12-21 12:56:29 +0530

    Working on Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'My First Angular';
+  courseChoice = 0;
+  courses : any[] = [
+    { duration:'10 hours' , name:'Typescript'},
+    { duration:'20 hours' , name:'Angular'},
+    { duration:'13 hours' , name:'Node JS'}
+  ]
+  changeTitle(){
+    if(this.title=='My First Angular')
+      this.title='This is my first angular';
+    else{
+      this.title='My First Angular';
+    }
+  }
+
+  selectedCourse(){
+    if(this.courseChoice > 3){
+      this.courseChoice=0;
+    }
+    this.courseChoice++;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
+    LoginComponent,
     AppComponent,
-    HelloComponent
+    HelloComponent 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent,HelloComponent]
+  bootstrap: [LoginComponent,AppComponent,HelloComponent]
 })
 export class AppModule { }
